1b8de68e7da388ba5105b721ef35b319f4e93588
enzomero <jonnyenzo@gmail.com>
2019-05-13 09:55:37 +0300

    no comment

--- view-app/src/main/java/com/jonny/controller/CommonController.java
+++ view-app/src/main/java/com/jonny/controller/CommonController.java
@@ -1,7 +1,5 @@
 package com.jonny.controller;
 
-import com.jonny.service.CommonModelService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CommonController {
 
-    private final CommonModelService service;
-
-    @Autowired
-    public CommonController(CommonModelService service) {
-        this.service = service;
-    }
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(){
         return "index";
@@ -24,7 +15,6 @@ public class CommonController {
 
     @RequestMapping(value = "all", method = RequestMethod.GET)
     public String readAll(ModelMap modelMap){
-        service.readAll(modelMap);
-        return "main/all";
+        return "index";
     }
 }
